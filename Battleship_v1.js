d657f6338c6724243b912fedeba245c1b505951b
KURAHANA <obaleslorenz@gmail.com>
2025-03-09 20:39:03 -0700

    Add files via upload

---
+++ Battleship_v1.js
@@ -0,0 +1,39 @@
+// Decleared variables for the game 
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess; 
+var hits = 0;
+var guesses = 0;
+
+var isSUNK = false;
+
+
+// Game Logic
+
+while (isSUNK == false) {
+    guess = prompt("Ready, Aim!, Fire! (Enter number 0-6):");
+
+    if(guess < 0 || guess > 6 ) {
+        alert("Please enter a valid cell number!")
+    }else {
+        guesses = guesses + 1;
+
+        if(guess == location1 || guess == location2 || guess == location3) {
+            hits += 1;
+            alert("Hit!")
+            
+            if(hits == 3) {
+                isSUNK = true; 
+                alert("You Sank my battleship!")
+            }
+        }else {
+            alert("MISS NA MISS KTA!")
+        }
+    }
+}
+
+var stats = "You sank my battleship at " + guesses + "guesses" + " and your accuracy is " + (3/guesses);
+
+alert(stats);
